28b9de81b18f4c66e711a101ac65e18b5776ba27
maximelafond <maxime.lafond@hotmail.com>
2017-06-11 01:21:21 -0400

    New look for the main page

--- MuseBox/forms.py
+++ MuseBox/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SelectionChansons(forms.Form):
-    TitreChanson1 = forms.CharField(label='Titre de la chanson numéro 1', max_length=100)
-    TitreChanson2 = forms.CharField(label='Titre de la chanson numéro 2', max_length=100)
-    TitreChanson3 = forms.CharField(label='Titre de la chanson numéro 3', max_length=100)
+    TitreChanson1 = forms.CharField(label='Title of the song #1', max_length=100)
+    TitreChanson2 = forms.CharField(label='Title of the song #2', max_length=100)
+    TitreChanson3 = forms.CharField(label='Title of the song #3', max_length=100)
